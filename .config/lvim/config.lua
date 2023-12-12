2ce8d99fb147dcd0e27eec808859fc15ae0a0a5e
Marius Reddig <marius@ruhestein.de>
2023-12-13 00:10:00 +0100

    V4: updated scripts

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -2,3 +2,8 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+
+lvim.keys.normal_mode["<A-Left>"] = ":tabprevious"
+lvim.keys.normal_mode["<A-Right>"] = ":tabnext"
+lvim.keys.normal_mode["<A-n>"] = ":tabnew"
+lvim.keys.normal_mode["<A-k>"] = ":tabclose"
